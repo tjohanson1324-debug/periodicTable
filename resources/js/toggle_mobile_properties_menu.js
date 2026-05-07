ec693bdfaebc00474066fa4a60b467a63347f3c8
tjohanson1324-debug <tjohanson1324@gmail.com>
2026-05-07 13:48:11 -0500

    Added code

--- resources/js/toggle_mobile_properties_menu.js
+++ resources/js/toggle_mobile_properties_menu.js
@@ -3,11 +3,13 @@ const togglePropertiesMenu = () =>
     if(document.querySelector("aside").classList.contains("visible"))
     {
     document.querySelector("aside").classList.remove("visible")
+    document.querySelector("#menuMobileCloseButton").textContent = "View"
     document.querySelector("section").classList.remove("hidden")
     }
 else
 {
     document.querySelector("aside").classList.add("visible")
+        document.querySelector("#menuMobileCloseButton").textContent = "Hide"
     document.querySelector("section").classList.add("hidden")
 }
 }
